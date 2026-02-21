5d6a04cc6b53df042aa9c9e655fcf3a6f237d7ea
tuandat <hoangdatlnbp@gmail.com>
2026-02-21 21:06:44 +0700

    feat: add DTO RegisterRequest

--- MicroNotify.Identity.Infrastructure/Migrations/20260221140223_AddRoleAndUserRoleEntities.Designer.cs
+++ MicroNotify.Identity.Infrastructure/Migrations/20260221140223_AddRoleAndUserRoleEntities.Designer.cs
@@ -4,6 +4,7 @@ using MicroNotify.Identity.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MicroNotify.Identity.Infrastructure.Migrations
 {
     [DbContext(typeof(IdentityDbContext))]
-    partial class IdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260221140223_AddRoleAndUserRoleEntities")]
+    partial class AddRoleAndUserRoleEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
